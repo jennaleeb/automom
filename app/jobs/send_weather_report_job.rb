4576a73aa98ab7b09055932ad5b566c3922e6a87
Jenna Blumenthal <jenna.blumenthal@shopify.com>
2017-05-15 19:34:26 -0400

    use string instead of symbol

--- app/jobs/send_weather_report_job.rb
+++ app/jobs/send_weather_report_job.rb
@@ -2,8 +2,8 @@ class SendWeatherReportJob
   # queue_as :default
   include Sidekiq::Worker
 
-  def perform(family_member_id)
-    family_member = FamilyMember.find(family_member_id)
+  def perform(params = {})
+    family_member = FamilyMember.find(params['family_member_id'])
     WeatherReportMailer.greet(family_member).deliver_now
   end
 end
